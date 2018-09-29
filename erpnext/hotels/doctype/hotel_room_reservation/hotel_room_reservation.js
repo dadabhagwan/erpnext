eb2495faec350f5614c7daf1de73fcac3a631ff5
Vijay <vijay_wm@yahoo.com>
2018-09-29 14:51:24 +0530

    [WIP] Create sales invoice from reservation

--- erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.js
+++ erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.js
@@ -2,67 +2,162 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Hotel Room Reservation', {
-	refresh: function(frm) {
-		if(frm.doc.docstatus == 1){
-			frm.add_custom_button(__("Make Invoice"), ()=> {
-				frm.trigger("make_invoice");
+
+	onload: function (frm) {
+		frm.fields_dict['room'].get_query = function (doc, cdt, cdn) {
+			return {
+				query: "erpnext.hotels.utils.get_available_rooms",
+				filters: { 'from_date': doc.from_date, 'to_date': doc.to_date, 'item': doc.item }
+			}
+		}
+	},
+
+	refresh: function (frm) {
+		if (frm.is_new()) {
+			erpnext.hotels.hotel_room_reservation.set_default_values(frm);
+		}
+
+		erpnext.hotels.hotel_room_reservation.setup_custom_actions(frm);
+
+	},
+
+	validate: function (frm) {
+		if (frm.doc.room && !frm.doc.room_status) {
+			frm.set_value("room_status", "Booked");
+		}
+	},
+
+	from_date: function (frm) {
+		erpnext.hotels.hotel_room_reservation.recalculate_rates(frm);
+	},
+
+	to_date: function (frm) {
+		erpnext.hotels.hotel_room_reservation.recalculate_rates(frm);
+	},
+
+	item: function (frm) {
+		// let days = frappe.datetime.get_diff(frm.doc.to_date, frm.doc.from_date);
+		//TODO: Prevent change if room already checked in, because rates will be affected
+	},
+
+
+});
+
+frappe.ui.form.on('Hotel Room Reservation Item', {
+
+	item: function (frm, doctype, name) {
+		erpnext.hotels.hotel_room_reservation.recalculate_rates(frm);
+	},
+
+	qty: function (frm) {
+		erpnext.hotels.hotel_room_reservation.recalculate_rates(frm);
+	}
+
+});
+
+frappe.provide('erpnext.hotels');
+
+erpnext.hotels.hotel_room_reservation = {
+
+	set_default_values: function (frm) {
+		frm.set_value("from_date", frappe.datetime.nowdate());
+		frm.set_value("to_date", frappe.datetime.add_days(frappe.datetime.nowdate(), 1));
+
+		frm.set_value("guest_name", "Test Customer 1");
+		frm.set_value("customer", "Darshanarthi");
+		frm.set_value("company", frappe.defaults.get_default('company'));
+	},
+
+	setup_custom_actions: (frm) => {
+
+		if (frm.doc.item && frm.doc.room) {
+			if (frm.doc.room_status == "Booked") {
+				frm.page.add_action_item(__("Check In"), function () {
+					erpnext.hotels.hotel_room_reservation.checkin(frm);
+				});
+			}
+			if (frm.doc.room_status === "Checked In") {
+				frm.page.add_action_item(__("Check Out"), function () {
+					erpnext.hotels.hotel_room_reservation.checkout(frm);
+				});
+			}
+		}
+
+		frm.page.add_action_item(__("Add to Group"), function () {
+			erpnext.hotels.hotel_room_reservation.show_group_dialog(frm);
+		});
+
+		if (!frm.doc.sales_invoice) {
+			frm.page.add_action_item(__("Make Invoice"), function () {
+				erpnext.hotels.hotel_room_reservation.make_sales_invoice(frm);
 			});
 		}
+
+		frm.page.add_action_item(__("Cancel Reservation"), function () {
+			erpnext.hotels.hotel_room_reservation.cancel_reservation(frm);
+		});
 	},
-	from_date: function(frm) {
-		frm.trigger("recalculate_rates");
-	},
-	to_date: function(frm) {
-		frm.trigger("recalculate_rates");
-	},
-	recalculate_rates: function(frm) {
-		if (!frm.doc.from_date || !frm.doc.to_date
-			|| !frm.doc.items.length){
+
+	checkin: (frm) => {
+		if (!frm.doc.room) {
+			frappe.msgprint(__("Please select a room for reservation."))
 			return;
 		}
+		frm.set_value('room_status', 'Checked In');
+		let days = frappe.datetime.get_diff(frm.doc.to_date, frm.doc.from_date);
+		frm.add_child("items", {
+			"item": frm.doc.item,
+			"qty": days
+		})
+		frm.refresh_field("items");
+		erpnext.hotels.hotel_room_reservation.recalculate_rates(frm);
+	},
+
+	checkout: (frm) => {
+		console.log('');
+		frappe.call({
+			"method": "erpnext.hotels.doctype.hotel_room_reservation.hotel_room_reservation.checkout",
+			"args": { "hotel_room_reservation": frm.doc, is_group: 0 }
+		}).done((r) => {
+			frm.set_value("room_status", "Checked Out");
+			frappe.run_serially([
+				() => frm.set_value("net_total", r.message.net_total),
+				() => frm.refresh_field("items")
+			]);
+		});
+	},
+
+	recalculate_rates: (frm) => {
+
+		if (!frm.doc.from_date || !frm.doc.to_date
+			|| !frm.doc.items.length) {
+			return;
+		}
+
 		frappe.call({
 			"method": "erpnext.hotels.doctype.hotel_room_reservation.hotel_room_reservation.get_room_rate",
-			"args": {"hotel_room_reservation": frm.doc}
-		}).done((r)=> {
+			"args": { "hotel_room_reservation": frm.doc }
+		}).done((r) => {
 			for (var i = 0; i < r.message.items.length; i++) {
 				frm.doc.items[i].rate = r.message.items[i].rate;
 				frm.doc.items[i].amount = r.message.items[i].amount;
 			}
 			frappe.run_serially([
-				()=> frm.set_value("net_total", r.message.net_total),
-				()=> frm.refresh_field("items")
+				() => frm.set_value("net_total", r.message.net_total),
+				() => frm.refresh_field("items")
 			]);
 		});
 	},
-	make_invoice: function(frm) {
-		frappe.model.with_doc("Hotel Settings", "Hotel Settings", ()=>{
-			frappe.model.with_doctype("Sales Invoice", ()=>{
-				let hotel_settings = frappe.get_doc("Hotel Settings", "Hotel Settings");
-				let invoice = frappe.model.get_new_doc("Sales Invoice");
-				invoice.customer = frm.doc.customer || hotel_settings.default_customer;
-				if (hotel_settings.default_invoice_naming_series){
-					invoice.naming_series = hotel_settings.default_invoice_naming_series;
-				}
-				for (let d of frm.doc.items){
-					let invoice_item = frappe.model.add_child(invoice, "items")
-					invoice_item.item_code = d.item;
-					invoice_item.qty = d.qty;
-					invoice_item.rate = d.rate;
-				}
-				if (hotel_settings.default_taxes_and_charges){
-					invoice.taxes_and_charges = hotel_settings.default_taxes_and_charges;
-				}
-				frappe.set_route("Form", invoice.doctype, invoice.name);
-			});
-		});
-	}
-});
 
-frappe.ui.form.on('Hotel Room Reservation Item', {
-	item: function(frm, doctype, name) {
-		frm.trigger("recalculate_rates");
+	make_sales_invoice: function (frm) {
+		// Using this instead of make_invoice. make_invoice not setting default values in Sales Invoice.	
+		frappe.model.open_mapped_doc({
+			method: "erpnext.hotels.doctype.hotel_room_reservation.hotel_room_reservation.make_sales_invoice",
+			frm: frm
+		})
 	},
-	qty: function(frm) {
-		frm.trigger("recalculate_rates");
-	}
-});
+
+}
+
+
+

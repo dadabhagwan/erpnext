45abc3a91d725a3f226879a66812bffdf48569f6
Vijay <vijay_wm@yahoo.com>
2018-10-01 23:26:13 +0530

    merged with staging

---
+++ erpnext/hotels/doctype/night_audit/night_audit.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+from frappe.utils import get_datetime_str, getdate
+from erpnext import get_default_currency
+
+
+class NightAudit(Document):
+    def autoname(self):
+        self.name = "NADT-%s-%s" % (self.company, self.date.replace("-", ""))
+
+    def validate(self):
+        self.items = []
+        filters = {"room_status": "Checked In",
+                   "company": self.company, "from_date": ["<=", self.date], "to_date": [">=", self.date]}
+        fields = ['room', 'name', 'item']
+        doclist = frappe.db.get_list(
+            "Hotel Room Reservation", filters=filters, fields=fields)
+        for d in doclist:
+            self.append("items", {
+                "room": d["room"],
+                "reservation": d["name"],
+                "item": d["item"],
+                "qty": 1,
+                "currency": get_default_currency(),
+                "rate": 100,
+                "amount": 100
+            })
+
+    def on_submit(self):
+        for d in self.items:
+            doc = frappe.get_doc('Hotel Room Reservation', d.reservation)
+            doc.post_room_and_tax(getdate(self.date))
+

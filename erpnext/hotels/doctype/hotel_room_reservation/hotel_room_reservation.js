487ecf4ce302d13b460f2f2c393b0bc34e87efd3
Vijay <vijay_wm@yahoo.com>
2018-10-09 16:38:08 +0530

    set item date for services

--- erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.js
+++ erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.js
@@ -123,9 +123,6 @@ erpnext.hotels.hotel_room_reservation = {
 	set_default_values: function (frm) {
 		frm.set_value("from_date", frappe.datetime.nowdate());
 		frm.set_value("to_date", frappe.datetime.add_days(frappe.datetime.nowdate(), 1));
-
-		// frm.set_value("guest_name", "Test Customer 1");
-		// frm.set_value("customer", "Darshanarthi");
 		frm.set_value("company", frappe.defaults.get_default('company'));
 	},
 

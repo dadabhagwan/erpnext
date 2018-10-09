4c42d62db5f76a04822453d1a53e8034064e6f81
Vijay <vijay_wm@yahoo.com>
2018-10-09 23:44:30 +0530

    added housekeeping entry on checkout

--- erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.js
+++ erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.js
@@ -132,12 +132,6 @@ erpnext.hotels.hotel_room_reservation = {
 
 	setup_custom_actions: (frm) => {
 
-		if (frm.doc.status === "Due Out") {
-			frm.page.add_action_item(__("Check Out"), function () {
-				erpnext.hotels.hotel_room_reservation.checkout(frm);
-			});
-		}
-
 		if (frm.doc.item && frm.doc.room) {
 			if (frm.doc.room_status == "Booked") {
 				frm.page.add_action_item(__("Check In"), function () {
@@ -150,6 +144,7 @@ erpnext.hotels.hotel_room_reservation = {
 				frm.page.add_action_item(__("Check Out"), function () {
 					erpnext.hotels.hotel_room_reservation.checkout(frm);
 				});
+
 
 				if (frm.doc.from_date == frappe.datetime.get_today() && !frm.doc.sales_invoice)
 					frm.page.add_action_item(__("Cancel Check In"), function () {

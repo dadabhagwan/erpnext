b311458cd5871bd551cb488ab2e255af50b4007f
Vijay <vijay_wm@yahoo.com>
2018-10-18 14:40:16 +0530

    Added confirmation for late checkout and early checkin

--- erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.js
+++ erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.js
@@ -145,7 +145,6 @@ erpnext.hotels.hotel_room_reservation = {
 					erpnext.hotels.hotel_room_reservation.checkout(frm);
 				});
 
-
 				if (frm.doc.from_date == frappe.datetime.get_today() && !frm.doc.sales_invoice)
 					frm.page.add_action_item(__("Cancel Check In"), function () {
 						erpnext.hotels.hotel_room_reservation.cancel_checkin(frm);

626c29136014c91e6a1d06397b098cedb87b1c61
Vijay <vijay_wm@yahoo.com>
2018-11-03 06:19:49 +0530

    reservation - member-room package validation changes

--- erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.js
+++ erpnext/hotels/doctype/hotel_room_reservation/hotel_room_reservation.js
@@ -30,9 +30,9 @@ frappe.ui.form.on('Hotel Room Reservation', {
 			}
 		});
 
-		// frm.fields_dict['profile_section'].collapse();
+		frm.fields_dict['profile_section'].collapse(false);
 		// frm.fields_dict['accounting_section'].collapse();
-		// frm.fields_dict['reservation_details_section'].collapse();
+		frm.fields_dict['reservation_details_section'].collapse(false);
 	},
 
 	refresh: function (frm) {
